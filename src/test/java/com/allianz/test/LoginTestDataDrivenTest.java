1934669a998edcb9340782b1e8b9e9d61cbdc79e
ShajilN <shajilnizarudeen@gmail.com>
2024-04-23 00:40:26 -0700

    added pages package

--- src/test/java/com/allianz/test/LoginTestDataDrivenTest.java
+++ src/test/java/com/allianz/test/LoginTestDataDrivenTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.allianz.base.AutomationWrapper;
+import com.allianz.pages.LoginPage;
 import com.allianz.utils.DataUtils;
 
 public class LoginTestDataDrivenTest extends AutomationWrapper{
@@ -12,9 +13,9 @@ public class LoginTestDataDrivenTest extends AutomationWrapper{
 	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class)
 	public void validLoginTest(String username, String password, String ExpectedHeader) {
 		
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[normalize-space()='Login'] ")).click();
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
 		String actualHeader=driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
 		//Assert the header - Dashboard
 		Assert.assertEquals(actualHeader, ExpectedHeader);
@@ -24,12 +25,10 @@ public class LoginTestDataDrivenTest extends AutomationWrapper{
 	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class)
 	public void invalidLoginTest(String username, String password, String expectedError)
 	{
-		//enter username
-		driver.findElement(By.name("username")).sendKeys(username);
-		//enter password 
-		driver.findElement(By.name("password")).sendKeys(password);
-		//click on login
-		driver.findElement(By.xpath("//button[normalize-space()='Login'] ")).click();
+
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
 		//Assert the error - Invalid credentials
 		String actualErrorText=driver.findElement(By.xpath("//p[contains(@class,'alert-content-text')]")).getText();	
 		Assert.assertEquals(actualErrorText, expectedError);		
